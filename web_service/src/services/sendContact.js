4d5bc4aa7495c76c7bdd25709894677b3ff3efb1
Anthony Rosario <anthonymrosario225@gmail.com>
2021-09-29 19:55:35 -0700

    .env file

--- web_service/src/services/sendContact.js
+++ web_service/src/services/sendContact.js
@@ -1,5 +1,5 @@
 export const sendContact = async (email, subject, message) =>{
-    await fetch('https://portfolio-ses.herokuapp.com/',{
+    await fetch('https://portfolio-aws-ses.herokuapp.com/',{
         method: 'POST',
         headers:{
             'Content-Type':'application/json' 
